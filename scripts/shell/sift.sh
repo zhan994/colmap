a4f3f37df2fb9c63d07f1f5095b044224fca0448
Zhihao Zhan <zhanzhihao_dt@163.com>
2024-10-16 19:32:41 +0800

    add brazil data intrinsics

--- scripts/shell/sift.sh
+++ scripts/shell/sift.sh
@@ -10,7 +10,7 @@ echo "$(log_time) feature extractor..."
 ./build/src/colmap/exe/colmap feature_extractor \
   --ImageReader.single_camera 1 \
   --ImageReader.camera_model OPENCV \
-  --ImageReader.camera_params "552.23198503638389,553.3633964901785,270,270,0.18643355235419351,-0.19986311220817557,-0.0011286505008237298,-0.00026876497068803516" \
+  --ImageReader.camera_params "3766.3120325946556,3766.4198382183604,2640,1978,-0.10311011108460813,-0.017784490784727422,-0.00012178249608126009,-0.00013355346085326488" \
   --SiftExtraction.use_gpu 1 \
   --SiftExtraction.max_image_size 1024 \
   --SiftExtraction.max_num_features 1000 \
