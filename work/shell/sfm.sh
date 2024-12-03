1058c3d0657944b78236f9faa7b491be5ece2aba
Zhihao Zhan <zhanzhihao_dt@163.com>
2024-12-03 23:56:50 +0800

    add CHANGELOG.txt for work
    update sift.sh & sfm.sh

--- work/shell/sfm.sh
+++ work/shell/sfm.sh
@@ -9,20 +9,22 @@ log_time() {
 
 PROJECT="${PWD}/proj"
 
+python3 work/python/camera_mask.py 960 540 200 250 ${PROJECT}/camera_mask.png
+
 echo "$(log_time) feature extractor ..."
 ./build/src/colmap/exe/colmap feature_extractor \
   --ImageReader.single_camera 1 \
+  --ImageReader.camera_mask ${PROJECT}/camera_mask.png \
   --ImageReader.camera_model OPENCV \
   --SiftExtraction.use_gpu 1 \
   --SiftExtraction.max_image_size 1024 \
-  --SiftExtraction.max_num_features 1000 \
+  --SiftExtraction.max_num_features 3000 \
   --database_path ${PROJECT}/database.db \
   --image_path ${PROJECT}/images
 echo "$(log_time) feature_extractor done."
 
 echo "$(log_time) feature matcher ..."
-./build/src/colmap/exe/colmap sequential_matcher \
-  --SequentialMatching.overlap 10  \
+./build/src/colmap/exe/colmap exhaustive_matcher \
   --SiftMatching.use_gpu 1 \
   --database_path ${PROJECT}/database.db
 echo "$(log_time) feature exhaustive_matcher done."
@@ -33,8 +35,8 @@ echo "$(log_time) colmap mapper ..."
   --Mapper.ba_refine_principal_point 1 \
   --Mapper.ba_refine_focal_length 1 \
   --Mapper.ba_refine_extra_params 1 \
-  --Mapper.ba_local_max_num_iterations 50 \
-  --Mapper.ba_global_max_num_iterations 100 \
+  --Mapper.ba_local_max_num_iterations 25 \
+  --Mapper.ba_global_max_num_iterations 50 \
   --database_path ${PROJECT}/database.db \
   --image_path ${PROJECT}/images \
   --output_path ${PROJECT}/sparse
--- work/shell/sift.sh
+++ work/shell/sift.sh
@@ -9,9 +9,12 @@ log_time() {
 
 PROJECT="${PWD}/proj"
 
+python3 work/python/camera_mask.py 960 540 200 250 ${PROJECT}/camera_mask.png
+
 echo "$(log_time) feature extractor ..."
 ./build/src/colmap/exe/colmap feature_extractor \
   --ImageReader.single_camera 1 \
+  --ImageReader.camera_mask ${PROJECT}/camera_mask.png \
   --ImageReader.camera_model OPENCV \
   --ImageReader.camera_params "550.0, 550.0, 480, 270, 0.12, -0.10, 0.00, 0.00" \
   --SiftExtraction.use_gpu 1 \
@@ -22,8 +25,7 @@ echo "$(log_time) feature extractor ..."
 echo "$(log_time) feature_extractor done."
 
 echo "$(log_time) feature matcher ..."
-./build/src/colmap/exe/colmap sequential_matcher \
-  --SequentialMatching.overlap 10  \
+./build/src/colmap/exe/colmap exhaustive_matcher \
   --SiftMatching.use_gpu 1 \
   --database_path ${PROJECT}/database.db
 echo "$(log_time) feature exhaustive_matcher done."
